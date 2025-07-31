4e983e85de17a95dbea0ae5cf55f820dd2de6788
Tarik Subasic <tarik.subasic@microbitdesign.com>
2025-07-31 14:44:28 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="algopython",
+    version="0.1",
+    packages=find_packages(),
+    author="Tarik Subasic",
+    description="Python package for controlling robot via serial commands",
+    python_requires=">=3.6",
+)    
